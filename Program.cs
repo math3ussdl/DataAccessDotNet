a6fb5fb46aeba74cc3fba7892dec6f6bf35d25d4
Hugo Lima <limabrot879@gmail.com>
2023-02-01 15:55:41 -0300

    feat: First Query with Dapper

--- Program.cs
+++ Program.cs
@@ -1,23 +1,13 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Dapper;
+using DataAccessDotNet.Models;
+using Microsoft.Data.SqlClient;
 
 const string connectionString = "Server=localhost,1433; Database=balta; User ID=sa; Password=MsSQL2023; Encrypt=False";
 
 using var connection = new SqlConnection(connectionString);
 
-connection.Open();
-Console.WriteLine("Connected!");
-
-using SqlCommand command = new();
-command.Connection = connection;
-command.CommandType = System.Data.CommandType.Text;
-command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-var reader = command.ExecuteReader();
-
-while (reader.Read())
+var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+foreach (var category in categories)
 {
-  // reader.GetGuid(int columnIndex)
-  // reader.GetString(int columnIndex)
-  // this reader contains methods for each c# data type
-  Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+  Console.WriteLine($"{category.Id} - {category.Title}");
 }
