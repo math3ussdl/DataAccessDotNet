bc8f78eea85d63a705b2dd652fb12845c843e1b0
Hugo Lima <limabrot879@gmail.com>
2023-02-01 17:26:20 -0300

    feat: Insert Category using dapper

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,6 +2,27 @@ namespace DataAccessDotNet.Models;
 
 public class Category
 {
+  public Category()
+  {
+    Id = Guid.NewGuid();
+  }
+
+  public Category(string title, string url, string summary, int order, string description, bool featured)
+  {
+    Id = Guid.NewGuid();
+    Title = title;
+    Url = url;
+    Summary = summary;
+    Order = order;
+    Description = description;
+    Featured = featured;
+  }
+
   public Guid Id { get; set; }
-  public string Title { get; set; } = default!;
+  public string Title { get; set; } = string.Empty;
+  public string Url { get; set; } = string.Empty;
+  public string Summary { get; set; } = string.Empty;
+  public int Order { get; set; }
+  public string Description { get; set; } = string.Empty;
+  public bool Featured { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,57 @@ using Microsoft.Data.SqlClient;
 
 const string connectionString = "Server=localhost,1433; Database=balta; User ID=sa; Password=MsSQL2023; Encrypt=False";
 
+var category = new Category(
+  title: "Amazon AWS",
+  url: "amazon",
+  summary: "AWS Cloud",
+  order: 8,
+  description: "Cloud AWS Services",
+  featured: false
+);
+
+string insertCategorySQL = @"
+  INSERT INTO [Category]
+  (
+    [Id],
+    [Title],
+    [Url],
+    [Summary],
+    [Order],
+    [Description],
+    [Featured]
+  )
+  VALUES (
+    @Id,
+    @Title,
+    @Url,
+    @Summary,
+    @Order,
+    @Description,
+    @Featured
+  )
+";
+
 using var connection = new SqlConnection(connectionString);
 
-var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-foreach (var category in categories)
+// Create a new category
+int rows = connection.Execute(insertCategorySQL, new
 {
-  Console.WriteLine($"{category.Id} - {category.Title}");
+  category.Id,
+  category.Title,
+  category.Url,
+  category.Summary,
+  category.Order,
+  category.Description,
+  category.Featured
+});
+
+Console.WriteLine($"Category with Id {category.Id} successfully created!");
+Console.WriteLine($"Rows affected: {rows}");
+
+// List all categories from database
+var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+foreach (var item in categories)
+{
+  Console.WriteLine($"{item.Id} - {item.Title}");
 }
