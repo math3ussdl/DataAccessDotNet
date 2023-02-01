129068a8f0678f52f8b326856c0cdac4e518c758
Hugo Lima <limabrot879@gmail.com>
2023-02-01 15:43:33 -0300

    chore: add sqlconnection implementation

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+
+const string connectionString = "Server=localhost,1433; Database=balta; User ID=sa; Password=MsSQL2023; Encrypt=False";
+
+using var connection = new SqlConnection(connectionString);
+
+connection.Open();
+Console.WriteLine("Connected!");
+
+using SqlCommand command = new();
+command.Connection = connection;
+command.CommandType = System.Data.CommandType.Text;
+command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+var reader = command.ExecuteReader();
+
+while (reader.Read())
+{
+  // reader.GetGuid(int columnIndex)
+  // reader.GetString(int columnIndex)
+  // this reader contains methods for each c# data type
+  Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+}
